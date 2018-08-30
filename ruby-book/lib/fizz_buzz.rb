440f31ecb3a6e2678d3684f036cb336ac19d1b60
Ryuto Fujita <rf.ryutooooo.0907@gmail.com>
2018-08-30 21:59:30 +0900

    wrote test

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -1,13 +1,12 @@
 def fizz_buzz(n)
 	if n % 15 == 0
+		'Fizz Buzz'
+	elsif n % 3 == 0
 		'Fizz'
-	elseif n % 5 == 0
+	elsif n % 5 == 0
 		'Buzz'
-	elseif n % 3 == 0
-		'FizzBuzz'
 	else
 		n.to_s
 	end
 end
-
 
